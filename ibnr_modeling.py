ab42c556c28e71137c6a440c303f9cc4f28bc989
henriquecorsini <henriquecorsini@gmail.com>
2026-07-13 19:53:28 -0300

    Update ibnr_modeling.py

--- ibnr_modeling.py
+++ ibnr_modeling.py
@@ -30,11 +30,22 @@ def normal_lambda_sampler(rng, loc, scale, size=()):
     lam = rng.normal(loc=loc, scale=scale, size=size)
     return np.clip(lam, 0.0, None)
 
-# ..
+# It is now time to introduce an model generating claim count data. This 
+# will be used in the future to test and compare methods which are used to
+# estimate IBNR numbers in real scenarios. Let me describe the hypotheses
+# of the model. I assume that the matter is dealt in the present and that 
+# the insurance portfolio is static in time, which is often reasonable in 
+# small time horizons. Hence, there is no need to introduce any counting 
+# variable besides n_occurrence_periods: staticness means that the whole
+# history of the portfolio is known. The estimation of the amount of claims
+# in development period k is done in a roundabout way by introducing a 
+# reporting pattern vector, which estimates the probability that an 
+# unreported claim be reported after k periods. It is reasonable to assume
+# that such reporting pattern vector be increasing: the longer a claim has 
+# remained unreported, the more likely it is that it should be reported in
+# the near future.
 
 def ibnr_claim_count_model(rng, n_occurrence_periods, reporting_pattern, lambda_sampler, *, sampler_kwargs = None):
-
-    # I did not introduce an n_development_periods variable because 
     
     sampler_kwargs = sampler_kwargs or {}
     reporting_pattern = np.asarray(reporting_pattern, dtype=float)
